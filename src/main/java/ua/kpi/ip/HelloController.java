426e8707abd880e2b96932d1158721b51e3752d5
Keing Chan <ihor.pismennyy@gmail.com>
2017-10-09 23:28:17 +0300

    dev

--- src/main/java/ua/kpi/ip/HelloController.java
+++ src/main/java/ua/kpi/ip/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
+import org.springframework.social.facebook.api.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +27,9 @@ public class HelloController {
         if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
             return "redirect:/connect/facebook";
         }
-
-        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        String [] fields = { "id", "email",  "first_name", "last_name" };
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+        model.addAttribute("facebookProfile", userProfile);
 //        PagedList<Post> feed = facebook.feedOperations().getFeed();
 //        model.addAttribute("feed", feed);
         return "hello";
