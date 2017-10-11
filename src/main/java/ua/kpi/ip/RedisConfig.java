1bac71623890a5c5aa2437283a7ab9fd61cb8c87
Keing Chan <ihor.pismennyy@gmail.com>
2017-10-12 00:49:46 +0300

    je

--- src/main/java/ua/kpi/ip/RedisConfig.java
+++ src/main/java/ua/kpi/ip/RedisConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -19,9 +18,6 @@ public class RedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-//        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-//        configuration.setHostName(host);
-//        configuration.setPort(port);
         JedisConnectionFactory conf = new JedisConnectionFactory();
         conf.setHostName(host);
         conf.setPort(port);
